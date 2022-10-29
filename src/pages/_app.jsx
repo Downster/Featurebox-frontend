614ae59da99d34ae8686ffdfe874d9ce515f1c93
Brendan Downing <downster@Brendans-Air.attlocal.net>
2022-10-28 18:22:57 -0700

    NExt-auth setup

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,11 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import { SessionProvider } from "next-auth/react"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <SessionProvider session={session}>
+ <Component {...pageProps} />
+  </SessionProvider>
+  )
 }
